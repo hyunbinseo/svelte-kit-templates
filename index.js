6fa0b872293a222519b5f72cf2ffdd56b41f9a59
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-06-03 21:53:31 +0900

    refactor: create bump script

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 import { execSync } from 'node:child_process';
 import { readFileSync, rmSync, writeFileSync } from 'node:fs';
 
-execSync('git checkout .');
-execSync('git pull');
-
-const latestTag = execSync('git describe --tags --abbrev=0').toString().trim();
-
-const pkg = (() => {
-	const name = 'create-svelte';
-	const version = execSync(`pnpm view ${name} version`).toString().trim();
-	return { name, version, spec: `${name}@${version}` };
-})();
-
-if (latestTag === `v${pkg.version}`) throw new Error('New version not found.');
-
-execSync(`pnpm add ${pkg.spec} --workspace-root`);
-
-// Reference https://github.com/sveltejs/kit/tree/main/packages/create-svelte#readme
-
-/**
- * @param {'checkjs' | 'typescript'} type
- * @returns svelte-create Options
- */
+/** @param {'checkjs' | 'typescript'} type */
 const generateOptions = (type) =>
 	/** @type {const} */ ({
 		types: type,
@@ -33,10 +13,21 @@ const generateOptions = (type) =>
 		vitest: false,
 	});
 
+const bump = process.env.npm_lifecycle_event === 'bump';
+
+if (bump) {
+	execSync('git checkout main');
+	execSync('git fetch origin');
+	execSync('git reset --hard origin/main');
+}
+
+execSync(`pnpm add create-svelte@latest --workspace-root`, { stdio: 'inherit' });
+
+const { version } = JSON.parse(readFileSync('node_modules/create-svelte/package.json', 'utf8'));
+const { create } = await import('create-svelte');
+
 rmSync('javascript', { recursive: true, force: true });
 rmSync('typescript', { recursive: true, force: true });
-
-const { create } = await import('create-svelte');
 
 await create('javascript', generateOptions('checkjs'));
 await create('typescript', generateOptions('typescript'));
@@ -51,16 +42,13 @@ writeFileSync(
 	'README.md',
 	readFileSync('README.md', { encoding: 'utf-8' }).replace(
 		/create-svelte@[\d\.]+\d/,
-		pkg.spec
+		`create-svelte@${version}`
 	)
 );
 
-try {
+if (bump) {
 	execSync('git add .');
-	execSync(`git commit -m "${pkg.spec}"`);
-	execSync('git push origin');
-	execSync(`git tag v${pkg.version}`);
-	execSync(`git push origin v${pkg.version}`);
-} catch {
-	console.log('git operations failed.');
+	execSync(`pnpm version ${version} -m "create-svelte@${version}" --force`, { stdio: 'inherit' });
+	execSync('git push');
+	execSync('git push --tags');
 }
