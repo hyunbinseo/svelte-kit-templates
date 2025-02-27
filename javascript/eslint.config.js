fc4b69f2f2c97f15974b655902fdbcf60ae35ec2
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-02-27 11:27:00 +0900

    sv@0.6.22

--- javascript/eslint.config.js
+++ javascript/eslint.config.js
@@ -4,13 +4,14 @@ import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
+import svelteConfig from './svelte.config.js';
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...svelte.configs["flat/recommended"],
+  ...svelte.configs.recommended,
   prettier,
   ...svelte.configs['flat/prettier'],
   {
@@ -18,6 +19,15 @@ export default [
 	  globals: {
 	    ...globals.browser,
 	    ...globals.node
+	  }
+	}
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.js"],
+
+    languageOptions: {
+	  parserOptions: {
+	    svelteConfig
 	  }
 	}
   }
