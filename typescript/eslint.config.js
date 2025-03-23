78cb08768aaf897eedf65f5a00454c2c67bb0fb4
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2025-03-23 21:16:33 +0900

    sv@0.7.0

--- typescript/eslint.config.js
+++ typescript/eslint.config.js
@@ -1,4 +1,4 @@
-import prettier from "eslint-config-prettier";
+import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
@@ -6,34 +6,36 @@ import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-  includeIgnoreFile(gitignorePath),
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs.recommended,
-  prettier,
-  ...svelte.configs.prettier,
-  {
-    languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node }
+		},
+		rules: { "no-undef": 'off' }
+	},
+	{
+		files: [
+			'**/*.svelte',
+			'**/*.svelte.ts',
+			'**/*.svelte.js'
+		],
+		ignores: ['eslint.config.js', 'svelte.config.js'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
+		}
 	}
-  },
-  {
-    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
-    ignores: ["eslint.config.js", "svelte.config.js"],
-
-    languageOptions: {
-	  parserOptions: {
-	    projectService: true,
-	    extraFileExtensions: ['.svelte'],
-	    parser: ts.parser,
-	    svelteConfig
-	  }
-	}
-  }
 );
