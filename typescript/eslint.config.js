388f5f9f5d74469a82ae68c499b7fe494c82fe18
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-06-04 16:33:38 +0900

    sv@0.15.4

--- typescript/eslint.config.js
+++ typescript/eslint.config.js
@@ -1,9 +1,8 @@
 import prettier from 'eslint-config-prettier';
 import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
