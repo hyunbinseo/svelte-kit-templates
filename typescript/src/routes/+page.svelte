cb892b25c99f4bef234db0d1b33188a02102c7e2
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-06-24 08:37:00 +0900

    chore: mre for svelte-esrap-142

--- typescript/src/routes/+page.svelte
+++ typescript/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+{#snippet a(disabled?: boolean)}
+	{disabled}
+{/snippet}
+
+{@render a()}
