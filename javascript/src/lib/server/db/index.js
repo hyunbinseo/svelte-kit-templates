b209699e6a0c6098da025c15b7dd84e4953c10c1
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-09-20 15:13:19 +0900

    sv@0.17.1

--- javascript/src/lib/server/db/index.js
+++ javascript/src/lib/server/db/index.js
@@ -1,10 +1,10 @@
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/node-sqlite';
+import { DatabaseSync } from 'node:sqlite';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = new DatabaseSync(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
